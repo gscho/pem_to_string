a75aec058d03f67770f59bef0553b7ca40c91142
gscho <greg.c.schofield@gmail.com>
2021-07-08 16:08:58 -0400

    It kinda works
    Signed-off-by: gscho <greg.c.schofield@gmail.com>

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+require 'json'
 require 'sinatra'
 
 set :protection, :except => :frame_options
@@ -6,4 +7,22 @@ set :port, 8080
 
 get '/' do
   erb :index
+end
+
+post '/convert-form' do
+  json = {'cert' => convert_pem(params[:pem]) }
+  body JSON.pretty_generate(json)
+end
+
+post '/convert' do
+  pem = request.body.read
+  json = { 'cert' => convert_pem(pem) }
+  body JSON.pretty_generate(json)
+end
+
+def convert_pem(pem_content)
+  converted_pem = pem_content
+  converted_pem.gsub(/\r/," ").split("\n").map do |line|
+    line.strip
+  end.join("\n") + "\n"
 end
